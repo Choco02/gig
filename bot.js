4100308d78858051d627c2f606e6ecedd0dfe113
Henrique Marino <henriquemarino48@gmail.com>
2019-05-06 22:31:11 -0300

    adicionado mais 3 requires, um trycatch para leitura de arquivos e alguns pequenos detalhes

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();//conexão com o cliente e as outras são todas requisições de libs/api
 const config = require('./config.json');
+const apikey = require('./apikey.json');
 const file = require('file-system');
 const fs = require('fs');
+const YouTube = require('simple-youtube-api');
+const yt = new YouTube(apikey.yt);
 
 //Evento da inicialização. Esse evento é importante, mesmo que não haja nada dentro, ele é necessário para que o bot seja inicializado
 client.on("ready",() =>{
@@ -30,25 +33,30 @@ client.on("message", async message=>{
     /*É sempre bom separar uma aplicação por funcionalidades, pra evitar que fique tudo em um só arquivo 
     e consequantemente você fique perdido por causa da ilegibilidade, por isso vamos usar a lib file-system*/
     //na linha a seguir é feita a leitura da pasta comandos
-    fs.readdir('./comandos', (err, files) => {
+    try {
+        fs.readdir('./comandos', (err, files) => {
     
-        if (err) console.log(err);
-        //aqui procuramos os carquivos que contém js no final. Ou seja, arquivos fonte de JavaScript
-        let jsFile = files.filter(f => f.split('.').pop() === "js");
-        console.log(jsfile);
-        //caso não haja nenhum arquivo js na pasta, será printado um aviso no console
-        if (jsFile.length <= 0) {
-            console.log("Comando não encontrado :c");
-            return;
-        }
-        //usamos um forEach pra percorrer a matriz de jsfile
-        jsFile.forEach((f) => {
-            let pull = require(`./comandos/${f}`);
-            console.log(`${f} carregado`);
-            //Essa condicional verifica se há um comando nos modulos igual ao que você executou. Caso sim, ele será executado
-            if (pull.config.aliases.includes(comando)) pull.run(client, message, args);
-        })
-    });
+            if (err) console.log(err);
+            //aqui procuramos os carquivos que contém js no final. Ou seja, arquivos fonte de JavaScript
+            let jsFile = files.filter(f => f.split('.').pop() === "js");
+            console.log(jsFile);
+            //caso não haja nenhum arquivo js na pasta, será printado um aviso no console
+            if (jsFile.length <= 0) {
+                console.log("Comando não encontrado :c");
+                return;
+            }
+            //usamos um forEach pra percorrer a matriz de jsfile
+            jsFile.forEach((f) => {
+                let pull = require(`./comandos/${f}`);
+                console.log(`${f} carregado`);
+                //Essa condicional verifica se há um comando nos modulos igual ao que você executou. Caso sim, ele será executado
+                if (pull.config.aliases.includes(comando)) pull.run(client, message, args, yt);
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    
 
     
     
