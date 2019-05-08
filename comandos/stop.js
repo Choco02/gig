ef6def057f3f0ddb6c2808294db0e017ae683265
Henrique Marino <henriquemarino48@gmail.com>
2019-05-08 12:54:38 -0300

    implementada a funcionalidade de parar a queue

--- comandos/stop.js
+++ comandos/stop.js
@@ -1,5 +1,11 @@
-exports.run = async (client, message, args) => {
-    
+exports.run = async (client, message, args, opts) => {
+
+    if(message.member.voiceChannel !==message.guild.me.voiceChannel)
+        return message.reply(" você precisa estar no mesmo canal de voz que o meu para fazer isso");
+        //saindo do canal de voz
+        message.member.voiceChannel.leave();
+        //deletando o objeto da guild
+        opts.map.delete(dispatcher.guildID);
 
 }
 
