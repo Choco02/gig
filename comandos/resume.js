b3ea3947aaece92caaf4a439b73695966cb5f9b8
Henrique Marino <henriquemarino48@gmail.com>
2019-05-08 12:45:20 -0300

    implementada a funcionalidade de retomar a musica pausada

---
+++ comandos/resume.js
@@ -0,0 +1,27 @@
+exports.run = async (client, message, args) => {
+    //aqui eu fiz quase que a mesma coisa, só usei o metodo resume pra retomar a musica
+  //Aqui nós pegamos objeto relacionado ao id da guild
+  let obter = opts.map.get(message.guild.id);
+
+  //Condição para caso não haja nada no objeto. Se não tem nada significa que não tem música, né?
+  if(!obter)
+      return message.reply(" não há músicas na fila para tocar");
+  //o retorno do if já diz o que ele faz. Caso o usuário não esteja no mesmo canal que o bot, ele não poderá pausar
+  if(!message.member.voiceChannel || message.member.voiceChannel !==message.guild.me.voiceChannel)
+      return message.reply(" você não está conectado a um canal de voz ou no mesmo que estou");
+  //Se a música já estiver pausada , será enviado um alerta 
+  if(!obter.dispatcher.paused)
+      return message.reply(" música já restá tocando");
+  
+  //E por fim, caso não aconteça nada das verificações anteriores, pausamos a música
+  obter.dispatcher.resume();
+
+  message.reply(` a música ${obter.queue[0].nome} foi retomada!`);
+
+}
+
+exports.config = {
+    name: 'resume',
+    aliases: ['resume']
+}
+
