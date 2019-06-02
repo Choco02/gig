dcbcf51e502ca5b47ded2d8d553ffd857282b765
Henrique Marino <henriquemarino48@gmail.com>
2019-06-02 18:15:49 -0300

    aletrada a lib de transmissão para a ytdl discord

--- comandos/play.js
+++ comandos/play.js
@@ -1,7 +1,8 @@
 const apikey = require('../apikey.json');
 const YouTube = require('simple-youtube-api');
 const yt = new YouTube(apikey.yt);
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core-discord');
+const moment = require('moment');
 const Discord = require('discord.js');
 //recomendo fortemente que leia a documentação do discord para que entenda algumas partes dos códigos
 //-> https://discord.js.org/#/docs/main/stable/class/StreamDispatcher
@@ -26,10 +27,6 @@ exports.run = async (client, message, args, opts) => {
                 //O que não pode acontecer de jeito nenhum, né?!
                 //leia sobre Maps aqui: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Map
                 let data = opts.map.get(message.guild.id) || {};
-                //verifica se o bot está em conectado, se não, ele será conectado
-                //if()
-                if(!data.connection)
-                data.connection = await message.member.voiceChannel.join();
                 //basicamente a mesma coisa, se não houver uma queue para aquela deterinada chave, será criada
                 if(!data.queue)
                 data.queue = [];
@@ -51,25 +48,78 @@ exports.run = async (client, message, args, opts) => {
                             anuncio:message.channel.id
                         });
                     }
+                    //verifica se o bot está em conectado, se não, ele será conectado
+                    if(!data.connection)
+                        data.connection = await message.member.voiceChannel.join();
+                    //se não ouver nenhum trasnmissão, será chamada a função de tocar
+                    if(!data.dispatcher)
+                        await play(client,opts,data);
+                    else{
+                        message.channel.send(playlist.title+" adicionada a fila!");
+                    }
                 }
                 if(!args[0].match(check)){
                     //procurando pela música
-                    let song = await yt.searchVideos(args.join(" "),1);
-                    data.queue.push({
-                        nome:song[0].title,
-                        qr:message.author,
-                        url:song[0].url,
-                        canal:song[0].channel.title,
-                        pub:song[0].publishedAt.toLocaleDateString(),
-                        anuncio:message.channel.id
-                    });
+                    let song = await yt.searchVideos(args.join(" "),5);
+                    let escolhida=null;
+                    let embed= new Discord.RichEmbed().setTitle('Escolha de 1 a 5')
+                    .addField('Resultados da busca:',song.map(r=>(song.indexOf(r)+1)+'-'+r.title).join('\n'))
+                    .setColor('RANDOM')
+                    message.channel.send(embed).then(async (msg)=>{
+                        let emoji = ['1⃣',"2⃣","3⃣","4⃣","5⃣"];
+                        for (let i=0;i<5;i++){
+                            
+                            
+                            await msg.react(emoji[i]);
+                        }
+                        const filter = (r, u) => r.me && u.id === message.author.id;
+                        const collector = msg.createReactionCollector(filter, {max: 1, time: 60 * 1000 });
+
+                        collector.on('collect', async (e)=>{
+                            msg.delete();
+                            
+                            switch (e.emoji.name) {
+                                case emoji[0]:
+                                    escolhida=song[0];
+                                    break;
+                                case emoji[1]:
+                                    escolhida=song[1];
+                                    break;
+                                case emoji[2]:
+                                    escolhida=song[2];
+                                    break;
+                                case emoji[3]:
+                                    escolhida=song[3];
+                                    break;
+                                case emoji[4]:
+                                    escolhida=song[4];
+                                    break;
+                                default:message.channel.send('deu ruim')
+                                    break;
+                            }
+                            
+                            data.queue.push({
+                                nome:escolhida.title,
+                                qr:message.author,
+                                url:escolhida.url,
+                                canal:escolhida.channel.title,
+                                pub:escolhida.publishedAt,
+                                anuncio:message.channel.id
+                            });
+                            //verifica se o bot está em conectado, se não, ele será conectado
+                            if(!data.connection)
+                                data.connection = await message.member.voiceChannel.join();
+                            //se não ouver nenhum trasnmissão, será chamada a função de tocar
+                            if(!data.dispatcher)
+                                await play(client,opts,data);
+                            else{
+                                message.channel.send(data.queue.slice(-1)[0].nome+" adicionada a fila!");
+                            }
+                        })
+                    })
+                    
                 }
-                //se não ouver nenhum trasnmissão, será chamada a função de tocar
-                if(!data.dispatcher)
-                    play(client,opts,data);
-                else{
-                    message.channel.send(data.queue.slice(-1)[0].nome+" adicionada a fila!");
-                }
+                
                 //E essa linha? Simples, se a já houver um expedidor, será somente adicionado um novo valor a chave da guild
                 //Você entenderá melhor nas próximas funções, guenta aí
                 opts.map.set(message.guild.id,data);
@@ -91,7 +141,7 @@ async function play(client,opts,data){
     
     //cria-se a transmissão. Nada melhor do que a própria documentação pra explicar:
     //https://discord.js.org/#/docs/main/stable/class/StreamDispatcher
-    data.dispatcher = await data.connection.playStream(ytdl(data.queue[0].url,{filter:'audioonly'}));
+    data.dispatcher = await data.connection.playOpusStream(await ytdl(data.queue[0].url));
     data.dispatcher.on('start', () => {
         data.dispatcher.player.streamingData.pausedTime = 0;
     });
@@ -100,14 +150,14 @@ async function play(client,opts,data){
     //aqui é um evento que será wmitido assim que o dispatcher terminar
     data.dispatcher.on('end',function(reason){
         //e mandamos o parametro. Esse this significa que será mandado o dispatcher atual
-        console.log('Music finalizada -> '+data.queue[0].nome+" razão = "+reason);
+        console.log('Musica finalizada! razão = '+reason);
         finish(client, opts, this);
         
     }).on('error', console.error);
     //pega o canal da instancia atual e envia ao mesmo uma mensagem
     if(data.dispatcher){
-        let embed = new Discord.RichEmbed().addField("Música:",data.queue[0].nome).addField("Canal:",data.queue[0].canal)
-        .addField("Publicado em:",data.queue[0].pub).setFooter("Adicionada por: "+data.queue[0].qr.tag,data.queue[0].qr.avatarURL)
+        let embed = new Discord.RichEmbed().addField("Música:",'['+data.queue[0].nome+']('+data.queue[0].url+')').addField("Canal:",data.queue[0].canal)
+        .addField("Publicado em:", moment(data.queue[0].pub).format("DD/MM/YYYY")).setFooter("Adicionada por: "+data.queue[0].qr.tag,data.queue[0].qr.avatarURL)
         .setColor('RANDOM');
         client.channels.get(data.queue[0].anuncio).send(embed);
     }
