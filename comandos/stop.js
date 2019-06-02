f88a6448e643d54dbeebb8189c2cd7275181eb9b
Henrique Marino <henriquemarino48@gmail.com>
2019-06-02 18:18:20 -0300

    alterada a forma que é parada a musica, para que não gere erros no console

--- comandos/stop.js
+++ comandos/stop.js
@@ -2,10 +2,11 @@ exports.run = async (client, message, args, opts) => {
 
     if(message.member.voiceChannel !==message.guild.me.voiceChannel)
         return message.reply(" você precisa estar no mesmo canal de voz que o meu para fazer isso");
-        //deletando o objeto da guild
-        opts.map.delete(message.guild.id);
-        //saindo do canal de voz
-        message.member.voiceChannel.leave();
+    
+    let fetched = opts.map.get(message.guild.id);
+    //zerando a queue
+    fetched.queue=[];
+    return fetched.dispatcher.end();
     console.log('Parado!');
 }
 
